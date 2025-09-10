aaa898e3ba5420fb195d4afc16834fb2c01a9125
seosooryeon <seosooryeon@localhost>
2025-09-10 16:46:52 +0900

    .

---
+++ src/main/java/com/etchapedia/EtchapediaApplication.java
@@ -0,0 +1,13 @@
+package com.etchapedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtchapediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtchapediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etchapedia/EtchapediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etchapedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtchapediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
