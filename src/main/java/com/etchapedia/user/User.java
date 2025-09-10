e0f29d67b41e1b054d4ff29db61f342dc297f677
seosooryeon <seosooryeon@localhost>
2025-09-10 16:50:12 +0900

    user table added.

---
+++ src/main/java/com/etchapedia/user/User.java
@@ -0,0 +1,25 @@
+package com.etchapedia.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="user")
+	@SequenceGenerator(name="user", sequenceName="seq_user_idx", allocationSize=1)
+	private Integer userIdx;
+	
+	private String email;
+	private String name;
+	private String password;
+	private String profile;
+
+}
