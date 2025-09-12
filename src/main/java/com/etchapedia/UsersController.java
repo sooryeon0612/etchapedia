f66155b54633063378448beae18b6bc684e00b4b
seosooryeon <seosooryeon@localhost>
2025-09-12 16:21:34 +0900

    conflict resolved.

--- src/main/java/com/etchapedia/UsersController.java
+++ src/main/java/com/etchapedia/UsersController.java
@@ -2,7 +2,6 @@ package com.etchapedia;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,23 +40,4 @@ public class UsersController {
 		uSvc.create(name, email, pw);
 		return "redirect:/";
 	}
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.etchapedia.user.UsersService;
-
-@Controller
-@RequestMapping("/user")
-public class UsersController {
-	@Autowired
-	private UsersService uSvc;
-
-	@GetMapping("/login")
-	public String login() {
-		return "login_form";
-	}
-	
->>>>>>> refs/heads/testSR
 }
