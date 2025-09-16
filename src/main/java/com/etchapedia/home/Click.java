2c7af9ae2fed023a9023a54d48f20316730dc321
seosooryeon <seosooryeon@localhost>
2025-09-16 18:59:16 +0900

    Click Entity added.

---
+++ src/main/java/com/etchapedia/home/Click.java
@@ -0,0 +1,33 @@
+package com.etchapedia.home;
+
+import com.etchapedia.user.Users;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Click {
+	@ManyToOne
+	@JoinColumn(name="book_idx")
+	private Book book;
+	
+	@ManyToOne
+	@JoinColumn(name="user_idx")
+	private Users user;
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="click")
+	@SequenceGenerator(name="click", sequenceName="seq_click_idx", allocationSize=1)
+	private Integer clickIdx;
+
+}
