28e6d00708b029e324f2b1e2d90706456ca5d2e1
seosooryeon <seosooryeon@localhost>
2025-10-01 17:22:13 +0900

    feat : 배포 설정 추가

--- src/main/java/com/etchapedia/EtchapediaApplication.java
+++ src/main/java/com/etchapedia/EtchapediaApplication.java
@@ -2,10 +2,17 @@ package com.etchapedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EtchapediaApplication {
+public class EtchapediaApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EtchapediaApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EtchapediaApplication.class, args);
 	}
